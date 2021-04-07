c39d20afc840a9ba5c92e93f5a125f5a0fe40793
Patanindev <tonson.pa@gmail.com>
2021-04-07 17:44:33 +0700

    rm

--- config.js
+++
@@ -1,9 +0,0 @@
-DATABASE_CONFIG = {
-        host: 'localhost',
-        port: '3306',
-        user: 'admin_pichub_servies_data',
-        password: 'MasterPichubLocal',
-        database: 'pichub_services_data'
-};
-
-module.exports = DATABASE_CONFIG;
