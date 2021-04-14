708b47bae325a18f10222ce033b29a3643b2bb97
Patanindev <tonson.pa@gmail.com>
2021-04-14 22:51:14 +0700

    feat: update getUserPost to response with formattedPictureResponse

--- models/readPictureByUserId.js
+++ models/readPictureByUserId.js
@@ -3,8 +3,19 @@ const mysql = require("mysql");
 
 const readPictureByUserId = (user_id) => {
     return new Promise((resolve, reject) => {
-        const SQL = `SELECT * FROM tbl_pictures 
-                    where user_id = ${mysql.escape(parseInt(user_id))}`;
+        const SQL = `SELECT 
+                        pic.id AS picture_id,
+                        pic.user_id     AS owner_id,
+                        u.profilename   AS profilename,
+                        pic.picturetitle,
+                        CONCAT(pic.uuid, '.', pic.filetype) AS filename,
+                        pic.description,
+                        pic.price,
+                        pic.stocklimits 
+                    FROM tbl_pictures AS pic
+                    INNER JOIN tbl_users AS u
+                    ON pic.user_id = u.id
+                    WHERE user_id = ${mysql.escape(parseInt(user_id))}`;
         dbMysql.query(SQL, (error, result) => {
         if (error) return reject(error);
         resolve(result);
